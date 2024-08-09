cd868ca2ced8f14e35dbe7826ff256df88a97077
Nils Jochem <nilsjochem@gmail.com>
2024-08-09 17:31:07 +0200

    aded new Traitbounds to Primitive

--- src/big_int/primitve.rs
+++ src/big_int/primitve.rs
@@ -1,13 +1,36 @@
+use std::fmt::{Binary, Debug, Display, LowerExp, LowerHex, Octal, UpperExp, UpperHex};
+
 use itertools::Either;
 
-pub trait Primitive: Copy + Eq + Ord {
+pub trait Primitive:
+    Copy
+    + Clone
+    + Debug
+    + Display
+    + Default
+    + Eq
+    + Ord
+    + Binary
+    + Octal
+    + LowerHex
+    + UpperHex
+    + LowerExp
+    + UpperExp
+{
     const BYTES: usize;
+    const BITS: usize;
     const ZERO: Self;
     const ONE: Self;
+    const MIN: Self;
+    const MAX: Self;
 
     type Pos: UNum<Neg = Self::Neg>;
     type Neg: INum<Pos = Self::Pos>;
 
+    fn from_be(self) -> Self;
+    fn from_le(self) -> Self;
+    fn to_be(self) -> Self;
+    fn to_le(self) -> Self;
     fn to_le_bytes(self) -> impl ExactSizeIterator<Item = u8> + DoubleEndedIterator;
     fn push_le_bytes(self, buf: &mut impl Extend<u8>)
     where
@@ -36,38 +59,45 @@ pub trait INum: Primitive {
 }
 
 macro_rules! implPrim {
-    ($pos_type: tt, $neg_type: tt, $bytes: literal) => {
+    (funcs, $bytes: literal) => {
+        const BYTES: usize = $bytes;
+        const BITS: usize = $bytes * 8;
+        const ZERO: Self = 0;
+        const ONE: Self = 1;
+        const MIN: Self = Self::MIN;
+        const MAX: Self = Self::MAX;
+
+        fn to_le_bytes(self) -> impl ExactSizeIterator<Item = u8> + DoubleEndedIterator {
+            self.to_le_bytes().into_iter()
+        }
+
+        implPrim!(relay, from_be, self, Self);
+        implPrim!(relay, from_le, self, Self);
+        implPrim!(relay, to_be, self, Self);
+        implPrim!(relay, to_le, self, Self);
+    };
+    (relay, $name: ident, self, $return: ident) => {
+        fn $name(self) -> $return {
+            Self::$name(self)
+        }
+    };
+    ($pos_type: ident, $neg_type: ident, $bytes: literal) => {
         impl Primitive for $pos_type {
-            const BYTES: usize = $bytes;
-
-            const ZERO: Self = 0;
-            const ONE: Self = 1;
-
             type Pos = $pos_type;
             type Neg = $neg_type;
-
-            fn to_le_bytes(self) -> impl ExactSizeIterator<Item = u8> + DoubleEndedIterator {
-                self.to_le_bytes().into_iter()
-            }
             fn select_sign(self) -> Either<Self::Pos, Self::Neg> {
                 Either::Left(self)
             }
+            implPrim!(funcs, $bytes);
         }
         impl Primitive for $neg_type {
-            const BYTES: usize = $bytes;
-
-            const ZERO: Self = 0;
-            const ONE: Self = 1;
-
             type Pos = $pos_type;
             type Neg = $neg_type;
 
-            fn to_le_bytes(self) -> impl ExactSizeIterator<Item = u8> + DoubleEndedIterator {
-                self.to_le_bytes().into_iter()
-            }
             fn select_sign(self) -> Either<Self::Pos, Self::Neg> {
                 Either::Right(self)
             }
+            implPrim!(funcs, $bytes);
         }
         impl UNum for $pos_type {
             fn try_neg(self) -> Option<Self::Neg> {
@@ -90,3 +120,10 @@ implPrim!(u16, i16, 16);
 implPrim!(u32, i32, 32);
 implPrim!(u64, i64, 64);
 implPrim!(u128, i128, 128);
+
+#[cfg(target_pointer_width = "64")]
+implPrim!(usize, isize, 64);
+#[cfg(target_pointer_width = "32")]
+implPrim!(usize, isize, 32);
+#[cfg(target_pointer_width = "16")]
+implPrim!(usize, isize, 16);
